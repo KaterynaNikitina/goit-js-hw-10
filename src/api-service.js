c2273ff9b8ec589e9cf93b665960948db2bcfc74
Katerina <Kateryna.e.nikitina@gmail.com>
2023-02-01 15:09:21 +0200

    new edit

--- src/api-service.js
+++ src/api-service.js
@@ -1,25 +1,28 @@
+
 export default class RestCountriesAPI {
-  constructor() {
-    this.searchQuery = '';
+  constructor(searchQuery) {
+    this.searchQuery = searchQuery;
   }
 
   getCountryInfo() {
     
     const url =
-      `https://restcountries.com/v3.1/name/${this.searchQuery}?fields=name,capital,population,flag,languages`;
+      `https://restcountries.com/v3.1/name/${this.searchQuery}?fields=name`;
 
     return fetch(url)
       .then(response => response.json())
-      .then(data => data.country);
-      
+      .then(data => data[0].name.common);           
+     
   }
 
   getQuery() {
     return this.searchQuery;
   }
 
-  setQuery() {
+  setQuery(newQuery) {
     this.searchQuery = newQuery;
   }
  
 }
+
+// ,capital,population,languages,flags
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import Notiflix from 'notiflix';
 import debounce from 'lodash.debounce';
-import RestCountiesAPI  from './api-service.js';
+import RestCountiesAPI from './api-service.js';
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
@@ -10,45 +10,44 @@ const countryInfo = document.querySelector('.country-info');
 
 countryInput.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
 
-// const countryList = document.querySelector('.country-list');
-
-
 const restCountriesAPI = new RestCountiesAPI();
 
 function onSearch(e) {
-  
   e.preventDefault();
-  
+
   restCountriesAPI.searchQuery = e.target.value.trim();
-  
-  restCountriesAPI.getCountryInfo()
+
+  // console.log(restCountriesAPI.searchQuery);
+
+  restCountriesAPI
+    .getCountryInfo()
     .then(createMarkup)
     .then(updateCountry)
     .catch(onFetchError)
-    .finally(() => restCountriesAPI.searchQuery = '')
+    .finally(() => (restCountriesAPI.searchQuery = ''));
 }
 
 function createMarkup({ name, capital, population, languages, flags }) {
-
-    const langs = Object.values(languages).join(', ');
+  const langs = Object.values(languages).join(', ');
 
   return 
-    `<img src="${flags.svg}" alt="name.official">
+  ` <img src="${flags.svg}" alt="name.official">
     <h2><${name.official}></h2>
     <p>Capital: ${capital}</p>
     <p>Population: ${population}</p>
     <p>Languages: ${langs}</p>`
-};
-  
-function updateCountry() {
+}
+
+function updateCountry(markup) {
   countryInfo.insertAdjacentHTML('beforeend', markup);
-} 
+}
 
 function onFetchError(error) {
   Notiflix.Notify.failure('Упс, что-то пошло не так');
 }
 
+// if (restCountriesAPI.length === 0) {
+//   div.innerHTML = ''
+// }
 
- // if (restCountriesAPI.length === 0) {
-  //   div.innerHTML = ''
-  // }
+// const countryList = document.querySelector('.country-list');
